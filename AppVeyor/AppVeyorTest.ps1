8c7ceab85937bdb99a2e9bbef5f814f6b7a026ce
Javy de Koning <javydekoning@gmail.com>
2016-08-01 17:09:27 +0200

    Fixes

--- AppVeyor/AppVeyorTest.ps1
+++ AppVeyor/AppVeyorTest.ps1
@@ -1,14 +1,14 @@
 ﻿#---------------------------------# 
 # Header                          # 
 #---------------------------------# 
-Write-Host 'Running AppVeyor build script' -ForegroundColor Yellow
+Write-Host 'Running AppVeyor test script' -ForegroundColor Yellow
 Write-Host "Current working directory: $pwd"
 
 #---------------------------------# 
 # Run Pester Tests                # 
 #---------------------------------# 
 $testResultsFile = '.\TestsResults.xml'
-$res             = Invoke-Pester -Script .\Tests\Demo.Tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
+$res             = Invoke-Pester -Script .\Tests\AppVeyorDemo.Tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
 
 Write-Host 'Uploading results'
 (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
